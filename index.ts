aabacb7637856d65c22a8d940cb5c78b05606d40
mez <mezanoor.r@gmail.com>
2020-04-16 19:34:00 +0100

    Initial commit

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { Server } from "./backend/server";
 import express from 'express';
 const app = express();
 
-const port = 8080;
+const port = parseInt(process.env.PORT) || 8080;
 
 const server = new Server(app);
 server.start(port);
